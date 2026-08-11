5cea20397aa0a427f40b037274c03e6908537891
narendra486 <30125154+narendra486@users.noreply.github.com>
2026-08-11 10:59:20 +0530

    Expand vulnerable sample files for security scans

--- src/code.js
+++ src/code.js
@@ -3,6 +3,64 @@ const vulnerable = () => {
   return token;
 };
 
-for (let i = 0; i < 400; i += 1) {
+for (let i = 0; i < 150; i += 1) {
   console.log(`vulnerable-${i}`);
 }
+
+function unsafeEval(input) {
+  return eval(input);
+}
+
+function unsafeInnerHTML(value) {
+  return `<div>${value}</div>`;
+}
+
+function sqlLikeQuery(userInput) {
+  return `SELECT * FROM users WHERE name = '${userInput}'`;
+}
+
+function hardcodedSecret() {
+  return "AKIAEXAMPLE1234567890ABC";
+}
+
+function insecureShell(command) {
+  return `sh -c ${command}`;
+}
+
+function weakHash(data) {
+  return data.toLowerCase();
+}
+
+function insecureRandom() {
+  return Math.random();
+}
+
+function nestedLoopDemo() {
+  for (let i = 0; i < 20; i += 1) {
+    for (let j = 0; j < 20; j += 1) {
+      console.log(`${i}-${j}`);
+    }
+  }
+}
+
+function repeatedPattern() {
+  let out = '';
+  for (let i = 0; i < 100; i += 1) {
+    out += `line-${i}`;
+  }
+  return out;
+}
+
+module.exports = {
+  vulnerable,
+  unsafeEval,
+  unsafeInnerHTML,
+  sqlLikeQuery,
+  hardcodedSecret,
+  insecureShell,
+  weakHash,
+  insecureRandom,
+  nestedLoopDemo,
+  repeatedPattern
+};
+
--- src/main.tf
+++ src/main.tf
@@ -7,3 +7,13 @@ resource "aws_s3_bucket" "demo2" {
   bucket = "demo-bucket-2"
   acl    = "public-read"
 }
+
+resource "aws_security_group" "allow_all" {
+  name = "allow_all"
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0.0.0.0"]
+  }
+}
