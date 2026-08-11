2fce01c459a01044a554eab70f306ca612721b2b
Narendra Palla <30125154+narendra486@users.noreply.github.com>
2026-08-11 12:26:26 +0530

    Update code.js

--- src/code.js
+++ src/code.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 
-// 1. SQL Injectionsmn 
+// 1. SQL Injectionsmnn
 app.get("/users", async (req, res) => {
   const username = req.query.username;
 
